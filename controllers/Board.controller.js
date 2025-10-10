731a74d0ae33d475705f8602fa621d4c777dac02
IbrohimMeliqulov <ibrohim.meliqulov2003@gmail.com>
2025-10-10 08:29:20 +0500

    new_updates

--- controllers/Board.controller.js
+++ controllers/Board.controller.js
@@ -1,2 +1,31 @@
 import pool from "../config/database.js";
 
+export const BoardController={
+    post:async function(req,res){
+        try{
+            const{title,user_id}=req.body
+            const values=[title,user_id]
+            const query=`INSERT INTO boards(title,user_id) VALUES($1,$2) RETURNING*`
+            const {rows}=await pool.query(query,values)
+            return res.status(200).send(rows)
+        }catch(err){
+            throw new Error(err)
+        }
+    },
+    getAll:async function(req,res){
+        try{
+            const 
+        }catch(err){
+            throw new Error(err)
+        }
+    },
+    getOne:async function(req,res){
+
+    },
+    put:async function (req,res){
+
+    },
+    delete:async function(req,res){
+
+    }
+}
